edc81fbf995c96c7d51d7a21e6ec345bf1c5279c
kartikey-pathak-au46 <kartikey963@gmail.com>
2022-10-30 10:35:49 +0530

    commit

--- node backend server/server.js
+++ node backend server/server.js
@@ -1,12 +1,37 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log("request is made");
-//   console.log(req.method);
-//   console.log(req.url);
+  // console.log(req);
+  // console.log(req.method);
+  // console.log(req.url);
 
+  // res.setHeader("Content-Type", "text/html");
+  // res.write("<h1>hello kartikey</h1>");
+  // res.write("<h1>hello everyone</h1>");
+  // res.end();
+  let path = "./html";
+  switch (req.url) {
+    case "/":
+      path += "/index.html";
+      break;
 
+    case "/about":
+      path += "/aboutUs.html";
+      break;
+    default:
+      path += "/404page.html";
+  }
 
+  fs.readFile("./html/index.html", (err, fileData) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.write(fileData);
+      res.end();
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
