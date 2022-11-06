374542b118bec85dbb1b42d0e59d0edfd008cd11
kartikey-pathak-au46 <kartikey963@gmail.com>
2022-11-06 09:56:46 +0530

    incomplete express mounting commit

--- node backend server/methods.js
+++ node backend server/methods.js
@@ -2,25 +2,86 @@ const express = require("express");
 const app = express();
 app.use(express.json()); //middleware fuc used in post req to turn data in json
 app.listen(3000);
-let users = {};
-app.get("/users", function (req, res) {
+let users = [
+  {
+    id: 1,
+    name: "Abhishek",
+  },
+  {
+    id: 2,
+    name: "kartikey",
+  },
+  {
+    id: 3,
+    name: "shiv",
+  },
+];
+
+const userRouter = express.Router();
+app.use("/users", userRouter);
+
+userRouter
+  .route("/")
+  .get(getUser)
+  .post(postuser)
+  .patch(postuser)
+  .delete(deleteUser);
+
+userRouter.route("/:id").get(getUserById);
+
+// // get req
+// app.get("/users", function (req, res) {
+//   res.send(users);
+// });
+
+// // post req
+
+// app.post("/users", function (req, res) {
+//   console.log(req.body);
+//   users = req.body;
+//   res.json({
+//     message: "data recieved",
+//     user: req.body,
+//   });
+// });
+
+// // update patch
+
+// app.patch("/users", function (req, res) {
+//   console.log("req.body->", req.body);
+//   let dataToBeUpdated = req.body;
+
+//   for (let key in dataToBeUpdated) {
+//     users[key] = dataToBeUpdated[key];
+//   }
+//   res.json({
+//     message: "data sucess",
+//   });
+// });
+
+// // delete req
+
+// app.delete("/users", function (req, res) {
+//   users = {};
+//   res.json({
+//     message: "data deleted",
+//   });
+// });
+
+function getUser(req, res) {
   res.send(users);
-});
+}
 
-// post req
-
-app.post("/users", function (req, res) {
+function postuser(req, res) {
   console.log(req.body);
   users = req.body;
   res.json({
     message: "data recieved",
     user: req.body,
   });
-});
+}
 
-// update patch
-
-app.patch("/users", function (req, res) {
+function updateuser(req, res) {
   console.log("req.body->", req.body);
   let dataToBeUpdated = req.body;
 
@@ -30,13 +91,26 @@ app.patch("/users", function (req, res) {
   res.json({
     message: "data sucess",
   });
-});
+}
 
-// delete req
-
-app.delete("/users", function (req, res) {
+function deleteUser(req, res) {
   users = {};
   res.json({
     message: "data deleted",
   });
-});
+}
+
+function getUserById(req, res) {
+  console.log(req.param.id);
+  let paramId = req.param.id;
+  let obj = {};
+  for (let i = 0; i < users.length; i++) {
+    if (users[i]["id"] == paramId) {
+      obj = users[i];
+    }
+  }
+  res.json({
+    message: "req received",
+    data: obj,
+  });
+}
